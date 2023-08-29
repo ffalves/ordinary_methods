83b23cc189f1e1acb12f3429d32ec4e898b54e60
Fefe Alves <monitore.falves@gmail.com>
2023-08-29 14:50:27 -0300

    commit analysis' script finished

--- Preparing dataset and working on ordinary methods.R
+++ Preparing dataset and working on ordinary methods.R
@@ -3,6 +3,7 @@
 
 ### Libraries that will be used (and that you should install them)
 library(dplyr)
+library(igraph)
 
 ######## PART 1
 ### Preparing dataset to analyse
@@ -47,6 +48,71 @@ borda = apply(borda, 1, sum)
 borda_df = data.frame(cbind(sofifa_id, borda))
 fifa_om = left_join(fifa_br, borda_df, by = 'sofifa_id')
 
+# You can see the players ranking by Borda's scores just ordering the borda's column
+
+
 ### Condorcet Count Method
 
+# Creating a Win matrix and initializing with zeros
+wins = matrix(0, ncol = nrow(matrix_players), nrow = nrow(matrix_players))
+rownames(wins) = matrix_df$sofifa_id
+colnames(wins) = matrix_df$sofifa_id
+
+# Creating a loop to count scores
+for ( player in 1:(ncol(matrix_players)-1) ) {
+  for ( next_player in (player+1):ncol(matrix_players) ) {
+    # Wins
+    win_player = 0
+    win_next_player = 0
+    
+    for ( duel in 1:nrow(matrix_players) ) {
+      if ( matrix_players[duel, player] > matrix_players[ duel, next_player] ) {
+        win_player = win_player+1
+      } else if ( matrix_players[duel, player] < matrix_players[duel, next_player] ) {
+        win_next_player = win_next_player+1
+      } else {
+        win_player = win_player+0
+        win_next_player = win_next_player+0
+      }
+    } 
+    
+    # Wins attribuitions
+    if ( win_player > win_next_player ) {
+      wins[player, next_player] = 1
+      wins[next_player, player] = -1
+    } else if ( win_player < win_next_player ) {
+      wins[player, next_player] = -1
+      wins[next_player, player] = 1
+    }
+  }
+}
+
+# We usually see Condorcet's results in a graph, so we have to build one 
+grafo = graph.adjacency(wins, mode = 'directed')
+
+# Finding and removing the isolated players
+players_off = which(degree(grafo, mode = 'total') == 0)
+new_grafo = delete_vertices(grafo, players_off)
+
+# Finding the winner
+winner = which.max(degree(new_grafo, mode = 'out'))
+
+# Changing the winner's color
+V(new_grafo)$color = 'grey'
+V(new_grafo)$color[winner] = 'green'
+
+# Graph visualization to see the winner
+plot(new_grafo)
+
+
+### Copeland Count Method
+
+# Adding player's scores from Condorcet's wins
+copeland = apply(wins, 1, sum)
+copeland = copeland[order(copeland, decreasing = T)]
+
+
+# Assigning the Copeland's score to each player
+copeland_df = data.frame(cbind(sofifa_id, copeland))
+fifa_om = left_join(fifa_om, copeland_df, by = 'sofifa_id')
 
